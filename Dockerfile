6a7376dc4838e30dcde047916a5aec5117fee8e1
Artem <thegunter@yandex.ru>
2022-05-01 00:02:30 +0300

    Added docker-compose.yaml, edited Dockerfile, edited database

--- Dockerfile
+++ Dockerfile
@@ -7,11 +7,13 @@ COPY ./.env /code
 
 WORKDIR /code
 
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+RUN apt-get update \
+  && apt-get -y install netcat gcc postgresql \
+  && apt-get clean
+
 RUN pip3 install -r requirements.txt
 
 WORKDIR /code/app
-
-EXPOSE 8000
-
-CMD ["uvicorn", "server.main:app", "--host=0.0.0.0", "--reload"]
-
--- app/database/database.py
+++ app/database/database.py
@@ -2,8 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from config.config import DB_NAME, DB_HOST, DB_PASS, DB_PORT, DB_USER
+import os
 
-SQL_ALCHEMY_DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+# SQL_ALCHEMY_DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+SQL_ALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
 
 engine = create_engine(
     SQL_ALCHEMY_DATABASE_URL,
